f2f8d3295c915d69474d89714b9e7f91f2b8c001
Zuo Yang <zuya@microsoft.com>
2018-01-02 22:16:46 +0800

    init APM project

---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+import { AuthService } from './user/auth.service';
+
+@Component({
+    templateUrl: './app/app.component.html'
+})
+export class AppComponent {
+    pageTitle: string = 'Acme Product Management';
+
+    constructor(private authService: AuthService) { }
+
+    logOut(): void {
+        this.authService.logout();
+        console.log('Log out');
+    }
+}
