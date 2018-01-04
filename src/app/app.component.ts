29a17db986246df925f6c1eb69b53dd47507dad0
Zuo Yang <zuya@microsoft.com>
2018-01-04 14:15:02 +0800

    Styling, Animating, and Watching Routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, Event, NavigationStart, NavigationEnd, NavigationError, NavigationCancel } from '@angular/router';
 
 import { AuthService } from './user/auth.service';
 
@@ -9,14 +9,31 @@ import { AuthService } from './user/auth.service';
 })
 export class AppComponent {
     pageTitle: string = 'Acme Product Management';
+    loading: boolean = true;
 
     constructor(private authService: AuthService,
-                private router: Router) { }
+        private router: Router) {
+        router.events.subscribe((routerEvent: Event) => {
+            this.checkRouterEvent(routerEvent);
+        });
+    }
 
     logOut(): void {
         this.authService.logout();
         console.log('Log out');
 
         this.router.navigateByUrl('/welcome');
+    }
+
+    checkRouterEvent(routerEvent: Event): void {
+        if (routerEvent instanceof NavigationStart) {
+            this.loading = true;
+        }
+
+        if (routerEvent instanceof NavigationEnd ||
+            routerEvent instanceof NavigationCancel ||
+            routerEvent instanceof NavigationError) {
+            this.loading = false;
+        }
     }
 }
